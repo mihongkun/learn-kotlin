ed824e573be54006f1d6f25e9902503c3b168082
Jackson Mi <1045262335@qq.com>
2017-09-28 07:59:29 +0800

    add something

--- package/src/main/kotlin/com/mihk/exception/ioException.kt
+++ package/src/main/kotlin/com/mihk/exception/ioException.kt
@@ -1,19 +1,20 @@
 package com.mihk.exception
 
 import java.io.BufferedReader
+import java.io.StringReader
 
 fun readNumber(reader:BufferedReader) :Int?{
     try {
         val line = reader.readLine()
         return Integer.parseInt(line)
     }catch (e: NumberFormatException) {
-        e.stackTrace
-        return null
+        throw e
     } finally {
         reader.close()
     }
 }
 
 fun main(args: Array<String>) {
-    
+
+    println(readNumber(BufferedReader(StringReader("yyy"))))
 }
